d43033f08fed7e9250127fca5dd02836b8343933
Krugou <59641229+Krugou@users.noreply.github.com>
2022-11-13 22:17:41 +0200

    done

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 'use strict';
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const username = 'foo';
+const password = 'bar';
+
+app.use(cookieParser());
+app.use(session({ secret: 'salainensaalis', cookie: { maxAge: 90000 } })); // 90000 ms = 90 s
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -9,5 +20,63 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
   res.render('home');
 });
+app.post('/login', (req, res) => {
+  if (req.body.password === password && req.body.username === username) {
+    req.session.logged = true;
+    res.redirect('/secret');
+  } else {
+    req.session.logged = false;
+    res.redirect('/form');
+  }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.get('/secret', (req, res) => {
+  if (req.session.logged) {
+    res.render('secret');
+  } else {
+    res.redirect('/form');
+  }
+});
+
+app.get('/setCookie/:clr', (req, res) => {
+  console.log(req.params.clr);
+  res.cookie('color', req.params.clr).send('Eväste valmis ' + req.params.clr);
+});
+
+app.get('/getCookie', (req, res) => {
+  console.log(req.cookies.color);
+  res.send('Evästeen sisältö ' + req.cookies.color);
+});
+
+app.get('/deleteCookie', (req, res) => {
+  res.clearCookie('color');
+  res.send('Eväste on poistettu');
+});
+
+app.listen(port, () =>
+  console.log(`Hei, kuuntelen osoitteessa localhost:${port} <-- porttia!`)
+);
+console.log('              __ ');
+console.log('             .¨  ¨. ');
+console.log('             :      :');
+console.log('             | _  _ |');
+console.log('          .-.|(o)(o)|.-.        _._          _._');
+console.log('         ( ( | .--. | ) )     .¨,_ ¨.      .¨ _,¨.');
+console.log('          ¨-/ (    ) -¨     / /¨ `  __ / /¨ `  ');
+console.log('           /   ¨--¨        / /     .¨  ¨./       ');
+console.log('            `"===="` /     `-`     : _  _ :      `-`');
+console.log('            `      /¨              |(o)(o)|');
+console.log('              `  /¨                |      |');
+console.log('              /`-.-`_             /         ');
+console.log('        _..:;._/V_./:;.._       /   .--.    ');
+console.log('      .¨/;:;:; /^ /:;:;:¨.     |  (    )  | ');
+console.log('     / /;:;:;:;| |/:;:;:;:     _  ¨--¨  /__');
+console.log('    / /;:;:;:;:;_/:;:;:;:;:  .¨  ¨-.__.-¨   `-.');
